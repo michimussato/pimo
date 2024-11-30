9af2a719292127aaaca10b7fe3a7f867e5d24298
Michael Mussato <michimussato@gmail.com>
2024-11-30 14:27:49 +1100

    refactor

--- src/pimo/pimo.py
+++ src/pimo/pimo.py
@@ -258,7 +258,7 @@ def set_inky_image(
 
     _max_border_value = int(min(inky.resolution) / 2) - 1
 
-    assert border < inky.resolution[0]/2 and border < inky.resolution[1]/2, f"Max border value for {inky.resolution} is {_max_border_value}."
+    assert border < _max_border_value, f"Max border value for {inky.resolution} is {_max_border_value}."
     assert all([0 <= i <= 255 for i in background_color])
     assert all([0 <= i <= 255 for i in border_color])
 
