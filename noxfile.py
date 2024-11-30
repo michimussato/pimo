a90e44da102785b125a5c931c2768a550ac970a1
Michael Mussato <michimussato@gmail.com>
2024-12-01 10:25:52 +1100

    nox lint

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,13 @@
+import os
 import nox
-import logging
 
-
-_logger = logging.getLogger(__name__)
 
 # https://www.youtube.com/watch?v=ImBvrDvK-1U&ab_channel=HynekSchlawack
 # https://codewitholi.com/_posts/python-nox-automation/
+
+
+nox.options.reuse_existing_virtualenvs = True
+# nox.options.sessions = ["lint", "tests"]
 
 
 VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
@@ -17,15 +19,16 @@ ENV = {
 }
 
 
-@nox.session(python=VERSIONS, tags=["tests"])
-def tests_no_cov(session):
-    session.install(".[tests]")
+@nox.session(python=VERSIONS, tags=["coverage"])
+def coverage(session):
+    """ Runs coverage """
 
-    session.run(
-        "pytest",
-        *session.posargs,
-        env=ENV,
-    )
+    session.install(".[coverage]")
+
+    session.run("coverage", "run", "--source", "src", "-m", "pytest", "-sv", env=ENV)  # ./.coverage
+    session.run("coverage", "report")  # report to console
+    # session.run("coverage", "xml")  # ./coverage.xml
+    # session.run("coverage", "html")  # ./htmlcov/
 
 
 @nox.session(python=VERSIONS, tags=["lint"])
@@ -41,7 +44,7 @@ def lint(session):
 
 
 @nox.session(python=VERSIONS, tags=["tests"])
-def tests_no_cov_after_lint(session):
+def tests_no_cov(session):
     session.install(".[tests]")
 
     session.run(
@@ -49,3 +52,33 @@ def tests_no_cov_after_lint(session):
         *session.posargs,
         env=ENV,
     )
+
+
+@nox.session(python=VERSIONS, tags=["release"])
+def release(session):
+    """Build and release to a repository"""
+    session.install(".[release]")
+
+    session.skip("Not implemented")
+
+    raise NotImplementedError
+
+    # pypi_user: str = os.environ.get("PYPI_USER")
+    # pypi_pass: str = os.environ.get("PYPI_PASS")
+    # if not pypi_user or not pypi_pass:
+    #     session.error(
+    #         "Environment variables for release: PYPI_USER, PYPI_PASS are missing!",
+    #     )
+    # session.run("poetry", "install", external=True)
+    # session.run("poetry", "build", external=True)
+    # session.run(
+    #     "poetry",
+    #     "publish",
+    #     "-r",
+    #     "testpypi",
+    #     "-u",
+    #     pypi_user,
+    #     "-p",
+    #     pypi_pass,
+    #     external=True,
+    # )
